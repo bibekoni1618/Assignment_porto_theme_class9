48cfceb61bd4e903f8ee87c1266a3b96e62f2112
ronjon <bibekoni1618@gmail.com>
2023-07-08 20:56:58 +0600

    porto theme assignment done

---
+++ src/home/data.js
@@ -0,0 +1,224 @@
+//<!-- ========== Clients  Start From Here ========== -->//
+const clients = [
+  {
+    id: 1,
+    brand: "Apple",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png",
+  },
+  {
+    id: 2,
+    brand: "google",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png",
+  },
+  {
+    id: 3,
+    brand: "flickr",
+    logo: "https://static.vecteezy.com/system/resources/previews/012/660/855/original/flickr-logo-on-transparent-isolated-background-free-vector.jpg",
+  },
+  {
+    id: 4,
+    brand: "behance",
+    logo: "https://cdn.freebiesupply.com/logos/large/2x/behance-4-logo-png-transparent.png",
+  },
+  {
+    id: 5,
+    brand: "dribble",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/3/32/Dribbble_logo.png",
+  },
+];
+//<!-- ========== Clients End From Here ========== -->//
+//<!-- ========== posts Start From Here ========== -->//
+
+const posts = [
+  [
+    {
+      id: 1,
+      title: " Lorem ipsum dolor sit amet consectetur, Corporis, animi?",
+      content:
+        " Lorem, ipsum dolor sit amet consectetur adipisicam accusantium, hic ad ut  numquam?",
+      post_time: {
+        month: "Jan",
+        date: 15,
+      },
+    },
+    {
+      id: 2,
+      title: " Lorem ipsum dolor sit ameg elit. Corporis, animi?",
+      content:
+        " Lorem, ipsum dolor sit amet conssantcere nemom optio obcaecati commodi eveniet numquam?",
+      post_time: {
+        month: "Mar",
+        date: 10,
+      },
+    },
+  ],
+  [
+    {
+      id: 3,
+      title: " Lorem ipsum dolor sit amet consectetur, adi animi?",
+      content:
+        " Lorem, ipsum . Maioreshic ad ut vel similiqm optio obcaecati commodi eveniet numquam?",
+      post_time: {
+        month: "Feb",
+        date: 18,
+      },
+    },
+    {
+      id: 4,
+      title: " Lorem ipsum dolor sit apis Corporis, animi?",
+      content:
+        " Lorem, ipsum doloe nemo eius veniam optio obcaecati commodi eveniet numquam?",
+      post_time: {
+        month: "Jul",
+        date: 23,
+      },
+    },
+  ],
+];
+//<!-- ========== posts End From Here ========== -->//
+//<!-- ========== testimonials Start From Here ========== -->//
+
+const testimonials = [
+  {
+    id: 1,
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus doloribus vitae laboriosam tenetur aspernatur officiis nemo corporis totam debitis delectus!",
+    client: {
+      name: "Ashraful Haque",
+      desg: "Instructor and Sr.Proggrammer",
+      img: "https://avatars.githubusercontent.com/u/63089244?v=4",
+    },
+  },
+  {
+    id: 2,
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus doloribus vitae laboriosam tenetur aspernatur officiis nemo corporis totam debitis delectus!",
+    client: {
+      name: "Zaheed Sabur",
+      desg: "Engineer Of Google Inc",
+      img: "https://media.licdn.com/dms/image/D4E03AQFcu1aYRRKKyA/profile-displayphoto-shrink_800_800/0/1674996318855?e=2147483647&v=beta&t=IRAGEqD2HEgoXevfh333RoLHOA741gl2p0Xq38A2Qhg",
+    },
+  },
+  {
+    id: 3,
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus doloribus vitae laboriosam tenetur aspernatur officiis nemo corporis totam debitis delectus!",
+    client: {
+      name: "Sundar Pichai",
+      desg: "CEO Of Google Inc",
+      img: "https://static.toiimg.com/photo/msid-96204681/96204681.jpg",
+    },
+  },
+];
+//<!-- ========== testimonials End From Here ========== -->//
+//<!-- ==========  Features Start From Here ========== -->//
+const features = [
+  //left side features
+  [
+    {
+      id: 1,
+      icon: "icon-support",
+      title: "24/7 Customer Support",
+      desc: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+    {
+      id: 2,
+      icon: "icon-doc",
+      title: "Static Design",
+      desc: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, m amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+    {
+      id: 3,
+      icon: "icon-social-google",
+      title: "Unlimited Fonts Support",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+    {
+      id: 4,
+      icon: "icon-pencil",
+      title: "Easy To Dynamic",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a. Aliquam, m amet consectetur adipisicing elit. Aliquam, magni?",
+    },
+  ],
+  // right side features
+  [
+    {
+      id: 5,
+      icon: "icon-layers",
+      title: "Awsome Slider Features",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+    {
+      id: 6,
+      icon: "icon-user",
+      title: "Awsome Icon Library",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+    {
+      id: 7,
+      icon: "icon-menu",
+      title: "Interactive Buttons",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+    {
+      id: 8,
+      icon: "icon-screen-desktop",
+      title: "Lightbox Perfomence",
+      desc: "  Lorem ipsum dolor, sit amet consectetur a. Aliquam, m amet consecsit amet consectetur a.magni?",
+    },
+  ],
+];
+//<!-- ==========  Features End From Here ========== -->//
+
+//<!-- ==========  Accordion Start From Here ========== -->//
+const accordions = [
+  {
+    id: 1,
+    title: "Ultimate Design",
+    description:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor esse quibusdam iusto saepe fugit temporibus porro laudantium vero molestias voluptate.",
+    icon: "icon-diamond",
+    acc_id: function () {
+      let id = this.title.split(" ");
+      return id.join("-");
+    },
+    target_link: "https://github.com/deveripon",
+  },
+  {
+    id: 2,
+    title: "Unlimited Demos",
+    description:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor esse quibusdam iusto saepe fugit temporibus porro laudantium vero molestias voluptate.",
+    icon: "icon-grid",
+    acc_id: function () {
+      let id = this.title.split(" ");
+      return id.join("-");
+    },
+    target_link: "https://github.com/deveripon",
+  },
+  {
+    id: 3,
+    title: "Nice Portfolios",
+    description:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor esse quibusdam iusto saepe fugit temporibus porro laudantium vero molestias voluptate.",
+    icon: "icon-diamond",
+    acc_id: function () {
+      let id = this.title.split(" ");
+      return id.join("-");
+    },
+    target_link: "https://github.com/deveripon",
+  },
+  {
+    id: 1,
+    title: "Contact Form",
+    description:
+      "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor esse quibusdam iusto saepe fugit temporibus porro laudantium vero molestias voluptate.",
+    icon: "icon-bubble",
+    acc_id: function () {
+      let id = this.title.split(" ");
+      return id.join("-");
+    },
+    target_link: "https://github.com/deveripon",
+  },
+];
+//<!-- ==========  Accordion End From Here ========== -->//
---
+++ src/team/data.js
@@ -0,0 +1,117 @@
+const team_category = [
+  "Full Steak Devs",
+  "Front End Devs",
+  "Back End Devs",
+  "Merketing",
+  "SEO",
+];
+const team_member = [
+  {
+    id: 1,
+    name: "Monoronjon Roy",
+    skill: "Mern Steak Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://scontent.fdac24-2.fna.fbcdn.net/v/t1.18169-9/1507107_248500488607584_9147875075508571701_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=ba80b0&_nc_eui2=AeHrMw14UgleCRZRK63eQZ0t8bpzvg_ZZVPxunO-D9llU_IGo35ah8nLjo3WK_q0SrJQsMjT5uWUlW5PCeBu317Y&_nc_ohc=BvwA7vRZz8UAX-tp4ab&_nc_ht=scontent.fdac24-2.fna&oh=00_AfAFEG_uo4nXYRN1N9hMDhvNA8PQJcJxScCCSKOJ3ri8kA&oe=64D0E838",
+    category: "full steak devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 2,
+    name: "Annay Morrison",
+    skill: "Laravel Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://jangada-dideias.pt/wp-content/uploads/2019/08/team-member-02.jpg",
+    category: "full steak devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 3,
+    name: "Richard Nixon",
+    skill: "Php Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-11.png",
+    category: "back end devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+    },
+  },
+  {
+    id: 4,
+    name: "Larra Williams",
+    skill: "React Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png",
+    category: "front end devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 5,
+    name: "Criss Morish",
+    skill: "IOS Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-8-1.png",
+    category: "full steak devs",
+    social: {
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 6,
+    name: "Jofree Peter k",
+    skill: "Mern Steak Developer",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://www.lascruces.org/wp-content/uploads/2017/02/team_member_01-540x540.jpg",
+    category: "full steak devs",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 7,
+    name: "Andy Willamson Pitter",
+    skill: "Marketing Expert",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-9.png",
+    category: "Merketing",
+    social: {
+      facebook: "https://www.facebook.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    id: 8,
+    name: "Alexa Andres",
+    skill: "SEO Expert",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac ligula mi, non suscipitaccumsan.",
+    photo:
+      "https://www.lascruces.org/wp-content/uploads/2017/02/team_member_02-540x540.jpg",
+    category: "seo",
+    social: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+];
